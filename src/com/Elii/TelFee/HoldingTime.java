6dee79b576dd60a857233623610bec5bad2f601f
Bravehyy <1147655179@qq.com>
2020-06-03 09:55:07 +0800

    实验三最终版

---
+++ src/com/Elii/TelFee/HoldingTime.java
@@ -0,0 +1,88 @@
+package com.Elii.TelFee;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+ 
+public class HoldingTime {
+	private String startingTime;//起始输入时间
+	private String endingTime;//结束输入时间
+	private long minute;//通话时长
+	public Date startT;
+	public Date endT;
+	public  HoldingTime(String startingTime,String endingTime){
+		this.startingTime = startingTime;
+		this.endingTime = endingTime;
+		this.startT = StrToDate(this.startingTime);
+		this.endT = StrToDate(this.endingTime);
+	}
+	
+	public Date StrToDate(String str) {
+		   SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+		   Date date = null;
+		   try {
+		    date = format.parse(str);
+		   } catch (ParseException e) {
+		    e.printStackTrace();
+		   }
+		   return date;
+		}
+	public String DateFormate(Date date){
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss E");
+		String sdate = format.format(date);
+		return sdate;
+	}
+	public long getHoldingTime(){
+		long between;
+		Calendar cal1 = Calendar.getInstance();
+		cal1.setTime(startT);
+		Calendar cal2 = Calendar.getInstance();
+		cal2.setTime(endT);
+		
+		between = cal2.getTime().getTime()-cal1.getTime().getTime();
+		if(between<0)
+			minute =( (cal2.getTime().getTime()-cal1.getTime().getTime())/1000)/60;//得到通话时长
+		else
+			minute = (between/1000+59)/60;
+		//判断接通时间是否在发生转换的日期
+		if((cal1.get(Calendar.MONTH) ==Calendar.MARCH && cal1.get(Calendar.DATE) >= 29 && cal1.get(Calendar.DATE) <= 31 && cal1.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY)
+			|| ((cal1.get(Calendar.MONTH) ==Calendar.APRIL && cal1.get(Calendar.DATE) >= 1 && cal1.get(Calendar.DATE) <= 4 && cal1.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY)))
+		{
+			System.out.println("需要转换1");
+			if(cal1.get(Calendar.HOUR_OF_DAY)<2 && cal2.get(Calendar.HOUR_OF_DAY)>=3)
+				minute -=60;//时间从2点转换到3点减去1h
+		}else{
+			if((cal2.get(Calendar.MONTH) ==3 && cal2.get(Calendar.DATE) >= 29 && cal2.get(Calendar.DATE) <= 31 && cal2.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY)
+					|| ((cal2.get(Calendar.MONTH) ==4 && cal2.get(Calendar.DATE) >= 1 && cal2.get(Calendar.DATE) <= 4 && cal2.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY)))
+				//判断结束时间是否在转换的日期
+			{
+				if(cal2.get(Calendar.HOUR_OF_DAY)>3 )
+					minute -=60;
+			}
+		}
+		//将位于转换的时间段内的时间都看作已经转换之后的
+		if(cal1.get(Calendar.MONTH) ==Calendar.OCTOBER && cal1.get(Calendar.DATE) >= 25 && cal1.get(Calendar.DATE) <= 31 && cal1.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY)
+		{
+			System.out.println("需要转换2");
+			if(cal1.get(Calendar.HOUR_OF_DAY) ==2){
+						minute +=60;
+			}
+		}else{
+			if(cal2.get(Calendar.MONTH) ==Calendar.OCTOBER && cal2.get(Calendar.DATE) >= 25 && cal2.get(Calendar.DATE) <= 31 && cal2.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY)
+			{
+				if(cal2.get(Calendar.HOUR_OF_DAY)==2)
+				{
+						minute +=60;
+				}
+			}
+		}
+		if(minute>1800 || minute <=0){
+			System.out.println("时间错误，时限范围不对！");
+			return 0;
+		}else{
+			//System.out.println("间隔正确");
+			return minute;
+		}
+	}
+}
---
+++ src/com/Elii/TelFee/TelFeeTest.java
@@ -0,0 +1,26 @@
+package com.Elii.TelFee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class TelFeeTest {
+
+	@ParameterizedTest
+	@DisplayName("TelFee一般等价类测试")
+	@CsvSource({"'20200602030405','20200602030545',0.1","'20200602030505','20200603030505',143.0"})
+	void testWeakNormalEqual(String strT,String strE,Double result) {
+		assertEquals(TelFee.Fee(strT, strE),result,0.001);
+	}
+
+	@ParameterizedTest
+	@DisplayName("TelFee健壮等价类测试")
+	@CsvSource({"'20200602030405','20200602030405',0.0","'20200602030406','20200606030406',0.0",
+			    "'20200329021501','20200329021808',0.2","'20201025025920','20201025030612',5.7"})
+	void testWeakRobustEqual(String strT,String strE,Double result) {
+		assertEquals(TelFee.Fee(strT, strE),result,0.001);
+	}
+
+}
