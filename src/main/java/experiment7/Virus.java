b5e414948794d4ec93f31a0c24fc7d8ebdca238f
Bravehyy <1147655179@qq.com>
2020-06-10 10:58:34 +0800

    实验七

---
+++ src/main/java/experiment7/Virus.java
@@ -0,0 +1,16 @@
+package experiment7;
+
+
+public class Virus {
+	public static int classify(boolean c1,boolean c2,boolean c3){
+		if(c1==true&&c3==true){
+			return 1;
+		}else if(c1==true&&c3==false){
+			return 2;
+		}else if(c1==false&&c3==true){
+			return 3;
+		}else{
+			return 4;
+		}
+	}
+}
